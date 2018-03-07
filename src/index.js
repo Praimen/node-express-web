7b899f4c09f36ec5e360dc2df05f0209e97f68f6
Tommie <praimen@gmail.com>
2018-03-07 00:14:21 -0600

    testing proxy

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ var path = require('path');
 
 var router = require('./router');
 var birds = require('./birds');
+
+var httpProxy = require('http-proxy');
+var apiProxy = httpProxy.createProxyServer();
+var serverOne = 'http://165.227.109.107:3000';
+
 var options = {
   dotfiles: 'ignore',
   etag: false,
@@ -24,15 +29,22 @@ app.set('view engine', 'pug');
 app.use(compression())
 app.set('views', './views');
 app.use(express.static(path.join(__dirname,'public'),options));
-app.use('/', router);
-app.use('/birds', birds);
+/*app.use('/', router);
+app.use('/birds', birds);*/
 
 
 app.get('/', (req, res) => res.render('index', {title: 'Hey', message: 'Hello World!'}));
 
 
+app.all("/app1/*", function(req, res) {
+  console.log('redirecting to Server1');
+  apiProxy.web(req, res, {target: serverOne});
+});
+
+/*
+
 app.get('/user', function (req, res, next) {
- /*res.sendFile(path.join(__dirname +'/public/special.html')) */
+ /!*res.sendFile(path.join(__dirname +'/public/special.html')) *!/
  res.send('back to the top')
 });
 
@@ -44,6 +56,7 @@ app.get('/ab?cd', function (req, res) {
   res.json({cat:"meow"})
 })
 
+*/
 
 
 
