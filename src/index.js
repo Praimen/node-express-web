a8a8d2cb8a5d98e97d69262624771813d8c8ea2b
Tommie <praimen@gmail.com>
2018-03-07 13:07:26 -0600

    removing some routes to differe from subdomain

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const compression = require('compression')
 const express = require('express');
+const app = express();
 var subdomain = require('express-subdomain');
 var dropletRoute = express.Router();
-const app = express();
 var path = require('path');
-
-var router = require('./router');
+var httpProxy = require('http-proxy');
 var birds = require('./birds');
 
-var httpProxy = require('http-proxy');
+/*var router = require('./router');*/
+
 var apiProxy = httpProxy.createProxyServer();
 var serverOne = 'http://165.227.109.107:3000';
 
@@ -24,49 +24,22 @@ var options = {
   }
 }
 
-
-
 app.set('view engine', 'pug');
-
-app.use(compression())
 app.set('views', './views');
+app.use(compression());
 app.use(express.static(path.join(__dirname,'public'),options));
-/*app.use('/', router);*/
-app.use('/birds', birds);
 
-dropletRoute.all('/',function(req, res){
+
+dropletRoute.all('/*',function(req, res){
         apiProxy.web(req, res, {target: serverOne});
     }
 );
 
+/*app.use('/', router);*/
 app.use(subdomain('droplet', dropletRoute));
-
-
+app.use('/birds', birds);
 app.get('/', (req, res) => res.render('index', {title: 'Hey', message: 'Hello World!'}));
 
-
-
-/*
-app.all("/app1/!*", function(req, res) {
-  console.log('redirecting to Server1');
-  apiProxy.web(req, res, {target: serverOne});
-});
-
-
-app.get('/user', function (req, res, next) {
- /!*res.sendFile(path.join(__dirname +'/public/special.html')) *!/
- res.send('back to the top')
-});
-
-app.get('/update', function (req, res) {
-  res.send('Got a PUT request at /update')
-})
-
-app.get('/ab?cd', function (req, res) {
-  res.json({cat:"meow"})
-})
-
-*/
 
 
 
