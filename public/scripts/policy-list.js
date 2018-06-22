327aea909db3c373176d25a10a8c79daf44c134e
Tommie <praimen@gmail.com>
2018-06-22 00:54:29 -0500

    see changes

--- public/scripts/policy-list.js
+++ public/scripts/policy-list.js
@@ -51,7 +51,12 @@ $(function(){
 
 
     $('select.policy-search').select2({
+        containerCssClass:'column is-6 select2-element',
+        containerCss:'width:""',
         minimumInputLength: 2,
+        dropdownAutoWidth:true,
+        dropdownCssClass:'select2-dropdown-element',
+        dropdownParent:$('.policy-search'),
         ajax: {
             delay: 250,
             url: function(){
